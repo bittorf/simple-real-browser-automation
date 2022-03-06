4835c7dd41fe61887175be64b0eee210707f7952
Bastian Bittorf <bb@npl.de>
2022-03-06 09:56:54 +0100

    boot-vm.sh: more helping text

--- boot-vm.sh
+++ boot-vm.sh
@@ -4,14 +4,37 @@ ACTION="$1"	# <empty>, debug, writeable
 
 IMAGE='image.bin'
 MEM='768m'
-PORTS="user,hostfwd=tcp::10022-:22,hostfwd=tcp::10080-:80,hostfwd=tcp::10059-:5900"
+PORT_HTTP=10080
+PORTS="user,hostfwd=tcp::10022-:22,hostfwd=tcp::${PORT_HTTP}-:80,hostfwd=tcp::10059-:5900"
 SNAPSHOT="-snapshot"
 DEBUG='-display none'
 
 if grep -q 'vmx\|svm' /proc/cpuinfo; then
 	echo "[OK] trying to run qemu in KVM mode (mybe needs sudo)"
-	qemu-system-x86_64 -cpu host -enable-kvm $DEBUG -nodefaults -m $MEM $SNAPSHOT -nic "$PORTS" -hda "$IMAGE"
+	qemu-system-x86_64 -cpu host -enable-kvm $DEBUG -nodefaults -m $MEM $SNAPSHOT -nic "$PORTS" -hda "$IMAGE" &
 else
 	echo "[OK] trying to run qemu (slow mode, no suitable cpuflags found)"
-	qemu-system-x86_64 -cpu host             $DEBUG -nodefaults -m $MEM $SNAPSHOT -nic "$PORTS" -hda "$IMAGE"
+	qemu-system-x86_64 -cpu host             $DEBUG -nodefaults -m $MEM $SNAPSHOT -nic "$PORTS" -hda "$IMAGE" &
+fi
+
+PID=$!
+echo "[OK] vm is booting"
+sleep 1
+vm_runs() { kill -0 "$PID"; }
+
+if vm_runs; then
+	while ! nc -z 127.0.0.1 $PORT_HTTP; do sleep 1; done
+
+	echo
+	echo "[OK] vm ready, you can enable SSH access with e.g.:"
+	echo "     curl http://127.0.0.1:$PORT_HTTP/action=startssh"
+	echo "  or speak with the API with e.g.:"
+	echo "     curl http://127.0.0.1:$PORT_HTTP/help"
+	echo
+	echo "(waiting for end of qemu with process-id $PID, e.g. with CTRL+C)"
+
+	while vm_runs; do sleep 1; done
+else
+	echo "[ERROR] on startup, process-id $PID died"
+	exit 1
 fi
